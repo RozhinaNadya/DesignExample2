7777f51298f64c516be709db577816a5160f6ef9
RozhinaNadya <zhuk.09@mail.ru>
2022-07-19 10:32:11 +0200

    Initial Commit

---
+++ DesignMainMenu/DesignMainMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignMainMenuApp.swift
+//  DesignMainMenu
+//
+//  Created by Надежда on 2022-07-19.
+//
+
+import SwiftUI
+
+@main
+struct DesignMainMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
